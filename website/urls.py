b87954816637fc80b290453c45c104c6047d9e47
guireroland623-bit <guireroland623@gmail.com>
2025-10-20 13:53:53 +0000

    Traveauxv pratique

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
+    path('', views.index, name='root'),
     path('l3info', views.index, name='home'),
     path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about'),
